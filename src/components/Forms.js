ea4ac2f3de41ac66f2b4cc94421ecda1cd782b8d
Welyson <welysonsr@gmail.com>
2022-06-11 17:12:45 -0300

    corrigindo build

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useContext, useState } from 'react';
 import StarContext from '../context/StarContext';
 import Remover from '../images/Remover.png';
